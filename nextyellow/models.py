3fa18ee13f780fb58041d8747b0aea0565d254a2
yujin <whitepigeon77@gmail.com>
2020-04-03 23:52:32 +0900

    hitcount 모델 구현

--- nextyellow/models.py
+++ nextyellow/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class HitCount(models.Model):
+    ip = models.CharField(max_length=15, default=None, null=True)  # ip 주소
+    date = models.DateField(default=timezone.now, null=True, blank=True)  # 조회수가 올라갔던 날짜 
